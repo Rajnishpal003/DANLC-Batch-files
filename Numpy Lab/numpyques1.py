e9dc8a33669d6ae2e9b704cff98997a807c35240
Shivangi Singh <s.shivangisingh2803@gmail.com>
2025-02-10 18:52:13 +0530

    Add files via upload

---
+++ Numpy Lab/numpyques1.py
@@ -0,0 +1,9 @@
+# 1. Write a NumPy program to create an array of 10 zeros, 10 ones, and 10 fives.
+import numpy as np
+
+array_zeroes=np.zeros(10)
+print("Array of 10 zeroes:-",array_zeroes)
+array_ones=np.ones(10)
+print("Array of 10 ones:-",array_ones)
+array_fives=np.full(10,5)
+print("Array of 10 fives:-",array_fives)
