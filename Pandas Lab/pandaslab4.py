20f1c95de70af1d4e641dfe984a76216b45a8e78
Shivangi Singh <s.shivangisingh2803@gmail.com>
2025-02-10 18:48:26 +0530

    Add files via upload

---
+++ Pandas Lab/pandaslab4.py
@@ -0,0 +1,26 @@
+#Lab4
+# Lab4: Write a Pandas program to select the 'name' and 'score' columns
+# from the following DataFrame.
+
+import pandas as pd
+import numpy as np
+
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily',
+            'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+            'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+            'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+            'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+
+#index labels
+index_labels=[0,1,2,3,4,5,6,7,8,9]
+#dictionary in dataframe with index labels
+df=pd.DataFrame(exam_data,index=index_labels)
+print("Original Data:\n",df)
+
+#select name and score columns
+print("-------------------------------------------")
+print("Specific Columns:\n",df[['name','score']])
+
+
+
+
